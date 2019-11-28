312b93f9371ff6d8be801f5f2514a49135d71d58
AndreaB2604 <andreabugin@hotmail.it>
2019-11-29 00:37:44 +0100

    Fixed colors for the python plot

--- plot_sol.py
+++ plot_sol.py
@@ -1,10 +1,9 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-import toyplot # pip install toyploy
+import matplotlib.cm as mplcm
+import matplotlib.colors as colors
 import networkx as nx
-
-import toyplot.pdf
 
 def init_nodes(file, p_coord):
 	line = file.readline()
@@ -48,62 +47,66 @@ def init_edges(file, edges, weights, h_weight):
 			edges.append(w_edge)
 			weights.append(weight)
 	np_edges = np.array(edges)
-	#print(np_edges)
 	np_weights = np.array(weights)
-	#print(np_weights)
 
 def plot_graph(p_coord, edges, weights, h_coord, h_weight):
 
+	fig, ax = plt.subplots()
+
+	nhosp = h_coord.shape[0]
 	G = nx.Graph()
 
 	for i in range(p_coord.shape[0]):
 		pos = (p_coord[i][0], p_coord[i][1])
-		G.add_node(i+1+h_coord.shape[0], pos=pos)
+		G.add_node(i+1+nhosp, pos=pos)
 
-	for i in range(h_coord.shape[0]):
+	for i in range(nhosp):
 		pos = (h_coord[i][0], h_coord[i][1])
-		G.add_node(i+1, pos=pos, color='red')
+		G.add_node(i+1, pos=pos)
 
-	edges[:,1] = edges[:,1] + h_coord.shape[0]
+	edges[:,1] = edges[:,1] + nhosp
 	weighted_edges = np.transpose(np.vstack((edges[:,0], edges[:,1], np.around(weights, decimals=2))))
+	#print(weighted_edges)
 
-	print(weighted_edges)
 	G.add_weighted_edges_from(weighted_edges)
 
-	color_map = []
-	for node in G:
-		if node <= h_coord.shape[0]:
-			color_map.append('blue')
-		else: color_map.append('green')
+	edge_color_map = G.edges()
+	#print(edge_color)
 
-	pos = nx.get_node_attributes(G,'pos')    
+	edge_color_map = (np.array(edge_color_map)[:,1]/(nhosp+1)).astype(float)
 
-	#nx.draw(G, pos, node_color = color_map, node_size=60)
-	nx.draw_networkx_nodes(G, pos, node_color=color_map, node_size=60)
-	nx.draw_networkx_edges(G, pos)
+	color_map = [(i/(nhosp+1)) if i <=nhosp else 1 for i in G.nodes()]
+
+	color_map = np.array(color_map).astype(float)
+
+	pos = nx.get_node_attributes(G, 'pos')
+
+	nx.draw_networkx_nodes(G, pos, node_size=60, node_color=color_map, cmap=mplcm.get_cmap("rainbow"), vmin=0.0, vmax=1.0)
+	nx.draw_networkx_edges(G, pos, edge_color=edge_color_map, edge_cmap=mplcm.get_cmap("rainbow"), edge_vmin=0.0, edge_vmax=1.0)
 
 	labels = nx.get_edge_attributes(G,'weight')
-	nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=4)
+	weights_bbox = dict(boxstyle="round, pad=0.3", fc="cyan", lw=0.1)
+	nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=4, label_pos=0.8,  bbox=weights_bbox)
 
 	px = p_coord[:,0]
 	py = p_coord[:,1] 
 	x_unique = np.unique(px)
 	y_unique = np.unique(py)
 
-	#plt.figure(figsize=(1, 1))
 	plt.xticks(np.arange(0, np.ceil(np.amax(px)), x_unique[1]-x_unique[0]))
 	plt.yticks(np.arange(0, np.ceil(np.amax(py)), y_unique[1]-y_unique[0]))
 
-	plt.axis('on')
+	ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+	
 	plt.grid(True)
+	ax.set_axisbelow(True)
 	plt.gca().set_aspect('equal', adjustable='box')
+	plt.savefig("solution_flow.pdf", format='pdf', bbox_inches='tight')
 	plt.show()
-
-	#plt.savefig("nodes.pdf", format='pdf', bbox_inches='tight')
 
 
 def main(toplot):
-	with open(toplot,"r") as file:
+	with open(toplot, "r") as file:
 		line = file.readline()
 		while (not(line.startswith("DIMENSION"))):
 			line = file.readline()
